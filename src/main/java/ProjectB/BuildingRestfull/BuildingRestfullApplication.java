ba7f7da901642e6f2e629c8dbaffe0b8ca13f65d
AndrePratama11 <andre.ong27@gmail.com>
2022-12-05 17:35:10 +0700

    075

---
+++ src/main/java/ProjectB/BuildingRestfull/BuildingRestfullApplication.java
@@ -0,0 +1,13 @@
+package ProjectB.BuildingRestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingRestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProjectB/BuildingRestfull/BuildingRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectB.BuildingRestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
